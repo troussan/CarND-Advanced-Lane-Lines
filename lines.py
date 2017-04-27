e4a4431117784c27efb33e1cd33d62afefdebe51
troussan <troussan@gmail.com>
2017-04-26 19:03:54 -0500

    Add sliding window

---
+++ lines.py
@@ -0,0 +1,108 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import threshold as thr
+import perspective as persp
+import sys
+
+def sliding_window(binary_warped, nwindows = 9, margin = 100, minpix = 50):
+    """
+    Parameters:
+    - binary_warped - bynary warped image
+    - nwindows - number of windows
+    - margin - width of the windows +/- margin
+    - minpix - minimum number of pixels found to recenter window
+
+    Returns left and right polynomial coefficients
+    """
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
+
+    # Find the peak of the left and right halves of the histogram
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    return left_fit, right_fit, left_lane_inds, right_lane_inds
+
+def test_sliding_window(img_path):
+    img = plt.imread(img_path)
+    t = thr.threshold(img)
+    M, _ = persp.perspective()
+    binary_warped = persp.transform(t, M)
+    left_fit, right_fit, left_lane_inds, right_lane_inds = sliding_window(binary_warped)
+    
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    plt.imshow(result)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.show()
+
+if __name__ == "__main__":
+    test_sliding_window(sys.argv[1])
---
+++ perspective.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import threshold as thr
+
+SOURCE_POINTS=np.float32([[255, 685],  [1055, 685],  [660, 435], [621, 435]])
+DESTINATION_POINTS=np.float32([[255, 720],  [1055, 720],  [1055, 0], [255, 0]])
+
+def perspective(src=SOURCE_POINTS, dst=DESTINATION_POINTS):
+    M = cv2.getPerspectiveTransform(src, dst)
+    R = cv2.getPerspectiveTransform(dst, src)
+    return M, R
+
+def transform(img, matrix):
+    return cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
+
+def test_perspective_color(img_path):
+    img = plt.imread(img_path)
+    M, _ = perspective()
+    transformed = transform(img, M)
+
+    cv2.polylines(img, [SOURCE_POINTS.reshape((-1,1,2)).astype(np.int32)], True, (255,0,0),3)
+    transf_pts = DESTINATION_POINTS.reshape((-1,1,2)).astype(np.int32)
+    cv2.polylines(transformed, [transf_pts], True, (0,0,255),3)
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=50)
+    ax2.imshow(transformed)
+    ax2.set_title(' Transformed result', fontsize=50)
+    plt.show()
+
+def test_perspective_threshold(img_path):
+    img = plt.imread(img_path)
+    t = thr.threshold(img)
+    M, _ = perspective()
+    transformed = transform(t, M)*255
+
+    transf_pts = DESTINATION_POINTS.reshape((-1,1,2)).astype(np.int32)
+    tr_color = np.array(cv2.merge((transformed, transformed, transformed)), np.uint8)
+    red = np.zeros_like(tr_color)
+    cv2.polylines(tr_color, [transf_pts], True, (255, 0, 0), 3)
+    output = cv2.addWeighted(tr_color,1.0, red, 0.5, 0.0)
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+    ax1.imshow(t, cmap='gray')
+    ax1.set_title('Original Image', fontsize=50)
+    ax2.imshow(output)
+    ax2.set_title(' Transformed result', fontsize=50)
+    plt.show()
+    
+if __name__ == "__main__":
+    if sys.argv[2] == 'color':
+        test_perspective_color(sys.argv[1])
+    else:
+        test_perspective_threshold(sys.argv[1])
